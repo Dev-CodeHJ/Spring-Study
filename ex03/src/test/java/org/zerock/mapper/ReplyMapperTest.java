81c4912921bdad0e7b81bc9c8c3c66e6bcd88a05
Admin <rlagnlwls0127@naver.com>
2023-11-06 18:02:34 +0900

    11/6 reply

--- ex03/src/test/java/org/zerock/mapper/ReplyMapperTest.java
+++ ex03/src/test/java/org/zerock/mapper/ReplyMapperTest.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
+@WebAppConfiguration
 @RunWith(SpringRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
